f1d3feeaeb09999f37094c018c7caa822d116c2a
ArturGoz <arturrm4@gmail.com>
2024-10-22 16:59:29 +0300

    fixed gun xsd and Gun class

--- src/main/java/artur/goz/Main.java
+++ src/main/java/artur/goz/Main.java
@@ -1,7 +1,15 @@
 package artur.goz;
 
+import artur.goz.generated.Gun;
+
+import java.util.List;
+
+import static artur.goz.xmlParsers.DOMParser.parseGuns;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Gun> guns = parseGuns("src/main/resources/gunsToParse.xml");
+        guns.forEach(m -> System.out.println(m.getModel()));
+        guns.forEach(m -> m.getTTC().getRange());
     }
 }
