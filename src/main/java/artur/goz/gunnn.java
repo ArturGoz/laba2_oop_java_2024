685ea8b033887ce4d309e3e541efb18399f85af6
ArturGoz <arturrm4@gmail.com>
2024-10-22 15:59:16 +0300

    xml xsd files added and get class from xsd file

---
+++ src/main/java/artur/goz/gunnn.java
@@ -0,0 +1,59 @@
+package artur.goz;
+
+public class gunnn {
+
+    private String id;
+
+    private String Model;
+    private String Handy;
+    private String Origin;
+   // private List<TTC> ttc;
+    private String Material;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public String getHandy() {
+        return Handy;
+    }
+
+    public String getOrigin() {
+        return Origin;
+    }
+
+
+    public String getMaterial() {
+        return Material;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
+
+    public void setHandy(String handy) {
+        Handy = handy;
+    }
+
+    public void setOrigin(String origin) {
+        Origin = origin;
+    }
+
+
+    public void setMaterial(String material) {
+        Material = material;
+    }
+
+    // Геттери і сеттери
+
+
+}
+
